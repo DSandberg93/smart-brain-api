76f694503eea45241cc699a0e27fd372773ff4b5
Daniel <danisan@stud.ntnu.no>
2019-03-04 15:16:15 +0100

    Initial commit and complete project upload

---
+++ controllers/image.js
@@ -0,0 +1,30 @@
+const Clarifai = require('clarifai');
+const secret = require('../secret');
+
+const app = new Clarifai.App({
+    apiKey: secret.clarifaiApiKey
+   });
+
+const handleApiCall = (req, res) => {
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => res.status(400).json('Unable to fetch data from API'));
+}
+
+const handleImage = (db) => (req, res) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0]);
+        })
+        .catch(err => res.status(400).json('Unable to get entry count'));
+}
+
+module.exports = {
+    handleImage: handleImage,
+    handleApiCall: handleApiCall
+}
